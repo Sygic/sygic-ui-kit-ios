9a3a5f817e1ed234d02bc0a9403333f35c028a17
Jakub Cali­k <jcalik@sygic.com>
2018-11-12 14:41:47 +0100

    Bottom sheet hide sample

--- Example/SygicUIKit/BottomSheetTestViewController.swift
+++ Example/SygicUIKit/BottomSheetTestViewController.swift
@@ -21,15 +21,29 @@ class BottomSheetTestViewController: UIViewController {
         }
     }
     
+    @objc func hideBottomSheet() {
+        bottomSheet.animateOut {
+            print("bottom sheet hidden")
+        }
+    }
+    
     private func setupHelperButtons() {
         let buttonShow = SYUIActionButton()
         buttonShow.translatesAutoresizingMaskIntoConstraints = false
         buttonShow.setup(with: ActionButtonViewModel(title: "Show bottom sheet"))
         buttonShow.addTarget(self, action: #selector(showBottomSheet), for: .touchUpInside)
         
+        let buttonHide = SYUIActionButton()
+        buttonHide.translatesAutoresizingMaskIntoConstraints = false
+        buttonHide.setup(with: ActionButtonViewModel(title: "Hide bottom sheet", style: .error))
+        buttonHide.addTarget(self, action: #selector(hideBottomSheet), for: .touchUpInside)
+        
         view.addSubview(buttonShow)
+        view.addSubview(buttonHide)
         buttonShow.topAnchor.constraint(equalTo: view.safeTopAnchor).isActive = true
         buttonShow.leadingAnchor.constraint(equalTo: view.safeLeadingAnchor).isActive = true
+        buttonHide.topAnchor.constraint(equalTo: buttonShow.bottomAnchor, constant: 8.0).isActive = true
+        buttonHide.leadingAnchor.constraint(equalTo: view.safeLeadingAnchor).isActive = true
     }
 
 }
