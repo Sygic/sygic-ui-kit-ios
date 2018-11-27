fda31245151d02d34677767adbb2d8737a38ca77
Marek Dovjak <mdovjak@sygic.com>
2018-11-27 13:40:38 +0100

    Remove SYUIPinViewViewModel

--- SygicUIKit/Classes/Views/Pin/SYUIPinView.swift
+++ SygicUIKit/Classes/Views/Pin/SYUIPinView.swift
@@ -1,51 +1,42 @@
 import UIKit
 import QuartzCore
 
-public protocol SYUIPinViewProperties {
-    var icon: String? { get }
-    var color: UIColor? { get }
-    var isSelected: Bool { get }
-    var animated: Bool { get }
-}
-
 public protocol SYUIPinViewDelegate: class {
-    func pinStateHasChanged(_ pin: SYUIPinView, isSelected: Bool)
+    func pinStateHasChanged(_ pin: SYUIPinView, isHighlighted: Bool)
     func pinWasTapped(_ pin: SYUIPinView)
 }
 
 public class SYUIPinView: UIView {
-    public var viewModel: SYUIPinViewProperties! {
+    public var icon: String? {
         didSet {
-            if let iconString = viewModel.icon, iconString.isLetterAtoZ, !iconView.isKind(of: SYUIPinViewLetter.self) {
+            if let iconString = icon, iconString.isLetterAtoZ, !iconView.isKind(of: SYUIPinViewLetter.self) {
                 iconView.removeFromSuperview()
                 iconView = SYUIPinViewLetter()
                 addIconView()
             }
             
-            if viewModel.icon != oldValue.icon {
-                iconView.icon = viewModel.icon
-            }
-            
-            if viewModel.color != oldValue.color {
-                backgroundView.pinColor = viewModel.color
-                iconView.iconColor = viewModel.color
-            }
-            
-            if viewModel.isSelected != oldValue.isSelected {
-                setSelected(viewModel.isSelected, animated: viewModel.animated)
-            }
+            iconView.icon = icon
+        }
+    }
+
+    public var color: UIColor? {
+        didSet {
+            backgroundView.pinColor = color
+            iconView.iconColor = color
         }
     }
     
-    private static let pinSize = CGSize(width: 48.0, height: 48.0)
+    public var isHighlighted = false {
+        didSet {
+            setHighlighted(isHighlighted, animated: animatedHighlight)
+        }
+    }
     
-    private var backgroundView = SYUIPinViewBackground()
-    private var iconView = SYUIPinViewIcon()
-    private var actionButton = UIButton()
+    public var animatedHighlight = false
     
     override public var frame: CGRect {
         get {
-            if let model = self.viewModel, model.isSelected {
+            if isHighlighted {
                 var selectedFrame = backgroundView.frame.union(iconView.frame)
                 selectedFrame.origin = CGPoint(x: super.frame.origin.x + selectedFrame.origin.x, y: super.frame.origin.y + selectedFrame.origin.y)
                 
@@ -62,9 +53,14 @@ public class SYUIPinView: UIView {
     
     public weak var delegate: SYUIPinViewDelegate?
     
+    private static let pinSize = CGSize(width: 48.0, height: 48.0)
+    private var backgroundView = SYUIPinViewBackground()
+    private var iconView = SYUIPinViewIcon()
+    private var actionButton = UIButton()
+    
     // MARK: - Life cycle
     
-    required public init() {
+    required public init(icon: String? = nil, color: UIColor? = nil, highlighted: Bool = false, animatedHighlight: Bool = false) {
         super.init(frame: CGRect(x: 0, y: 0, width: SYUIPinView.pinSize.width, height: SYUIPinView.pinSize.height))
         
         autoresizingMask = []
@@ -72,15 +68,21 @@ public class SYUIPinView: UIView {
         
         backgroundView.center = CGPoint(x: bounds.width/2.0, y: bounds.height/2.0)
         addSubview(backgroundView)
-
+        
         addIconView()
-
+        
         actionButton.frame = backgroundView.frame.union(iconView.frame)
         actionButton.addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
         addSubview(actionButton)
         
         backgroundView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
-        viewModel = SYUIPinViewViewModel()
+        
+        defer {
+            self.icon = icon
+            self.color = color
+            self.isHighlighted = highlighted
+            self.animatedHighlight = animatedHighlight
+        }
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -97,25 +99,25 @@ public class SYUIPinView: UIView {
     public func shouldShowNormalState() -> Bool {
         return true
     }
-
+    
     public func shouldShowSelectedState() -> Bool {
         return true
     }
-
+    
     // MARK: - Selection handling
-
+    
     override public func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        if self.viewModel.isSelected, backgroundView.frame.contains(point) {
+        if isHighlighted, backgroundView.frame.contains(point) {
             return actionButton
         }
         
         return super.hitTest(point, with: event)
     }
     
-    public func setSelected(_ selected: Bool, animated: Bool, completion: (() -> Void)? = nil) {
-        delegate?.pinStateHasChanged(self, isSelected: selected)
+    public func setHighlighted(_ highlighted: Bool, animated: Bool, completion: (() -> Void)? = nil) {
+        delegate?.pinStateHasChanged(self, isHighlighted: highlighted)
         
-        if selected {
+        if highlighted {
             superview?.bringSubview(toFront: self)
             
             if animated {
@@ -129,7 +131,7 @@ public class SYUIPinView: UIView {
                 completion?()
             }
         } else {
-
+            
             if animated {
                 UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
                     self.deselectedState()
@@ -146,7 +148,7 @@ public class SYUIPinView: UIView {
     private func selectedState() {
         backgroundView.alpha = shouldShowSelectedState() ? 1 : 0
         backgroundView.transform = CGAffineTransform.identity
-    
+        
         iconView.alpha = shouldShowSelectedState() ? 1 : 0
         iconView.transform = CGAffineTransform.identity
         iconView.center = CGPoint(x: bounds.width / 2.0, y: -22)
@@ -178,7 +180,7 @@ public class SYUIPinView: UIView {
         //shadows
         
         size.height += 8.0
-        if !viewModel.isSelected {
+        if isHighlighted {
             size.width += 10.0
             size.height += 6.0
         }
@@ -186,7 +188,7 @@ public class SYUIPinView: UIView {
         UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
         var image: UIImage? = nil
         if let context = UIGraphicsGetCurrentContext() {
-            if viewModel.isSelected {
+            if isHighlighted {
                 context.translateBy(x: 16.0, y: 48.0)
             } else {
                 context.translateBy(x: 5.0, y: 7.0)
