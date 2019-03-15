82bb2f9fc4e7c99c56fe780c4f39e2a96b400dc1
Jakub Kracina <jkracina@sygic.com>
2019-03-15 19:20:51 +0100

    Search Bar UI
    - Search Bar View (protocol & delegate)
    - Search Bar View Controller (delegate)

---
+++ SygicUIKit/Classes/Views/Search/SYUISearchBarController.swift
@@ -0,0 +1,76 @@
+//// SYUISearchBarController.swift
+//
+// Copyright (c) 2019 - Sygic a.s.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the &quot;Software&quot;), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+public protocol SYUISearchBarDelegate: class {
+    func search(textDidChange searchedText: String)
+    func searchDidBeginEditing()
+    func searchDidEndEditing()
+    func searchSearchButtonClicked()
+    func searchCancelButtonClicked()
+}
+
+public class SYUISearchBarController: UIViewController, SYUISearchBarDelegate {
+    
+    public weak var delegate: SYUISearchBarDelegate?
+    var searchBarView: SYUISearchBarProtocol = SYUISearchBarView()
+    
+    public override func loadView() {
+        if let searchView = searchBarView as? UIView {
+            view = searchView
+        }
+        searchBarView.searchBarDelegate = self
+    }
+    
+    public func prefillSearch(with text: String) {
+        guard let searchView = searchBarView as? SYUISearchBarView else { return }
+        searchView.searchBar.text = text
+    }
+    
+    public override func becomeFirstResponder() -> Bool {
+        return view.becomeFirstResponder()
+    }
+    
+    public override func resignFirstResponder() -> Bool {
+        return view.resignFirstResponder()
+    }
+    
+    public func search(textDidChange searchedText: String) {
+        delegate?.search(textDidChange: searchedText)
+    }
+    
+    public func searchDidBeginEditing() {
+        delegate?.searchDidBeginEditing()
+    }
+    
+    public func searchDidEndEditing() {
+        delegate?.searchDidEndEditing()
+    }
+    
+    public func searchSearchButtonClicked() {
+        delegate?.searchSearchButtonClicked()
+    }
+    
+    public func searchCancelButtonClicked() {
+        delegate?.searchCancelButtonClicked()
+    }
+    
+}
---
+++ SygicUIKit/Classes/Views/Search/SYUISearchBarView.swift
@@ -0,0 +1,79 @@
+//// SYUISearchBarView.swift
+//
+// Copyright (c) 2019 - Sygic a.s.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the &quot;Software&quot;), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+
+public protocol SYUISearchBarProtocol {
+    var searchBarDelegate: SYUISearchBarDelegate? { get set }
+}
+
+public class SYUISearchBarView: UIView, UISearchBarDelegate, SYUISearchBarProtocol {
+
+    public let searchBar = UISearchBar()
+    public weak var searchBarDelegate: SYUISearchBarDelegate?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        searchBar.delegate = self
+        searchBar.searchBarStyle = .minimal
+        searchBar.showsCancelButton = true
+    
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(searchBar)
+        searchBar.coverWholeSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func becomeFirstResponder() -> Bool {
+        return searchBar.becomeFirstResponder()
+    }
+    
+    public override func resignFirstResponder() -> Bool {
+        return searchBar.resignFirstResponder()
+    }
+    
+    public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchBarDelegate?.search(textDidChange: searchText)
+    }
+    
+    public func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBarDelegate?.searchDidBeginEditing()
+    }
+    
+    public func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBarDelegate?.searchDidEndEditing()
+    }
+    
+    public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBarDelegate?.searchSearchButtonClicked()
+    }
+    
+    public func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBarDelegate?.searchCancelButtonClicked()
+    }
+
+}
